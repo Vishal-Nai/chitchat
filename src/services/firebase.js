2d3402f9dac8bdeb3ae16e9d353a79ff4699df40
vishal <vishal@intellimedianetworks.net>
2020-05-08 11:25:09 +0530

    changes

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,14 +3,7 @@ import "firebase/auth";
 import "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCwR-daQaDjx209GJvAb_U8HLTqC3YrBkg",
-  authDomain: "chitchat-66d85.firebaseapp.com",
-  databaseURL: "https://chitchat-66d85.firebaseio.com",
-  projectId: "chitchat-66d85",
-  storageBucket: "chitchat-66d85.appspot.com",
-  messagingSenderId: "238404920302",
-  appId: "1:238404920302:web:12217920f586b9fd7424ff",
-  measurementId: "G-2N875LNNN4"
+  //firebase configuration
 };
 
 firebase.initializeApp(firebaseConfig);
